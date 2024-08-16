d650cda3421e014aafc1507e859ea70acacf60ef
Scott Shih <yujen91164@gmail.com>
2024-08-16 14:32:44 +0800

    Update index.js

--- themes/nav/index.js
+++ themes/nav/index.js
@@ -36,41 +36,13 @@ import TocDrawer from './components/TocDrawer'
 import TopNavBar from './components/TopNavBar'
 import CONFIG from './config'
 import { Style } from './style'
-import addFavorites from '@/components/addfavorite'
+import addFavorites from './components/addfavorite'
 
 const WWAds = dynamic(() => import('@/components/WWAds'), { ssr: false })
 
 // 主题全局变量
 const ThemeGlobalNav = createContext()
 export const useNavGlobal = () => useContext(ThemeGlobalNav)
-
-{
-function addFavorites(a) {
-    pageTitle = document.title;
-    pageURL = document.location;
-    try {
-        // Internet Explorer solution
-        eval("window.external.AddFa-vorite(pageURL, pageTitle)".replace(/-/g, ''));
-    } catch (e) {
-        try {
-            // Mozilla Firefox solution
-            window.sidebar.addPanel(pageTitle, pageURL, "");
-        } catch (e) {
-            // Opera solution
-            if (typeof(opera) == "object") {
-                a.rel = "sidebar";
-                a.title = pageTitle;
-                a.url = pageURL;
-                return true;
-            } else {
-                // The rest browsers (i.e Chrome, Safari)
-                alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
-            }
-        }
-    }
-    return false;
-}
-}
 
 /**
  * 基础布局
