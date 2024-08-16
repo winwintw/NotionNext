8a0fe51d72733810e97a9e54610dee9c7cdf9bec
Scott Shih <yujen91164@gmail.com>
2024-08-16 12:58:40 +0800

    Update index.js

--- themes/nav/index.js
+++ themes/nav/index.js
@@ -136,7 +136,7 @@ const LayoutBase = props => {
                     <MenuItem key={index} link={link} />
                   ))}
                 {
-                  <div class="nav-menu dark:text-neutral-400 text-gray-500 hover:text-black dark:hover:text-white text-sm text-gray w-full items-center duration-300 pt-2 font-light select-none flex justify-between cursor-pointer"><a class="dark:text-neutral-400 dark:hover:text-white font-bold w-full display-block" href="javascript:window.external.AddFavorite(’https://cmuhdnr.vercel.app/','桂圓生存手冊 cmuhdnr.vercel.app')"><i class="text-base fas fa-info mr-1"></i>加入書籤</a></div>
+                  <div class="nav-menu dark:text-neutral-400 text-gray-500 hover:text-black dark:hover:text-white text-sm text-gray w-full items-center duration-300 pt-2 font-light select-none flex justify-between cursor-pointer"><a class="dark:text-neutral-400 dark:hover:text-white font-bold w-full display-block" href="javascript:window.external.AddFavorite('https://cmuhdnr.vercel.app/','桂圓生存手冊 cmuhdnr.vercel.app')"><i class="text-base fa-solid fa-star mr-1"></i>加入書籤</a></div>
                     }
               </div>                
             </div>
