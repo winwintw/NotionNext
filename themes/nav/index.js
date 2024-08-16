1f17f2fdc1864619d7a710f30b7846b899a0497a
Scott Shih <yujen91164@gmail.com>
2024-08-16 14:22:28 +0800

    Update index.js

--- themes/nav/index.js
+++ themes/nav/index.js
@@ -43,6 +43,34 @@ const WWAds = dynamic(() => import('@/components/WWAds'), { ssr: false })
 const ThemeGlobalNav = createContext()
 export const useNavGlobal = () => useContext(ThemeGlobalNav)
 
+{
+function addFavorites(a) {
+    pageTitle = document.title;
+    pageURL = document.location;
+    try {
+        // Internet Explorer solution
+        eval("window.external.AddFa-vorite(pageURL, pageTitle)".replace(/-/g, ''));
+    } catch (e) {
+        try {
+            // Mozilla Firefox solution
+            window.sidebar.addPanel(pageTitle, pageURL, "");
+        } catch (e) {
+            // Opera solution
+            if (typeof(opera) == "object") {
+                a.rel = "sidebar";
+                a.title = pageTitle;
+                a.url = pageURL;
+                return true;
+            } else {
+                // The rest browsers (i.e Chrome, Safari)
+                alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
+            }
+        }
+    }
+    return false;
+}
+}
+
 /**
  * 基础布局
  * 采用左右两侧布局，移动端使用顶部导航栏
@@ -137,33 +165,7 @@ const LayoutBase = props => {
                   ))}
 
 {/* 書籤功能START */}
-{
-function addFavorites(a) {
-    pageTitle = document.title;
-    pageURL = document.location;
-    try {
-        // Internet Explorer solution
-        eval("window.external.AddFa-vorite(pageURL, pageTitle)".replace(/-/g, ''));
-    } catch (e) {
-        try {
-            // Mozilla Firefox solution
-            window.sidebar.addPanel(pageTitle, pageURL, "");
-        } catch (e) {
-            // Opera solution
-            if (typeof(opera) == "object") {
-                a.rel = "sidebar";
-                a.title = pageTitle;
-                a.url = pageURL;
-                return true;
-            } else {
-                // The rest browsers (i.e Chrome, Safari)
-                alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
-            }
-        }
-    }
-    return false;
-}
-}
+
                 { 
                   <div class="nav-menu dark:text-neutral-400 text-gray-500 hover:text-black dark:hover:text-white text-sm text-gray w-full items-center duration-300 pt-2 font-light select-none flex justify-between cursor-pointer"><a class="dark:text-neutral-400 dark:hover:text-white font-bold w-full display-block" href="javascript:void(0)" onClick="return addFavorites(this);"><i class="text-base fa-solid fa-star mr-1"></i>加入書籤</a></div>
                     }
