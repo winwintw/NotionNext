ba7dd3ab4aba1179c6e5e36165f0e5e3d90b8602
Scott Shih <yujen91164@gmail.com>
2024-08-18 01:52:39 +0800

    Update config.js

--- themes/commerce/config.js
+++ themes/commerce/config.js
@@ -2,18 +2,17 @@ const CONFIG = {
   // 封面大图
   COMMERCE_HOME_BANNER_ENABLE: true,
 
-  COMMERCE_TEXT_CENTER_TITLE: 'Product Center', // 中间产品区块标题
-  COMMERCE_TEXT_CENTER_DESCRIPTION:
-    'The vision of NotionNext is to help you effortlessly and seamlessly build your own website, amplifying the value of your brand.', // 中间产品区块文字描述
-  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: 'Product Categories', // 左侧产品分类标题
-  COMMERCE_TEXT_FOOTER_TITLE: 'Contact US', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
-  COMMERCE_TEXT_FOOTER_MENU_1: 'Product Center', // COMMERCE主题页脚左侧菜单标题1
-  COMMERCE_TEXT_FOOTER_MENU_2: 'About US', // COMMERCE主题页脚左侧菜单标题2
+  COMMERCE_TEXT_CENTER_TITLE: null, // 中间产品区块标题
+  COMMERCE_TEXT_CENTER_DESCRIPTION: null, // 中间产品区块文字描述
+  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: '文章分類', // 左侧产品分类标题
+  COMMERCE_TEXT_FOOTER_TITLE: '意見回饋', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
+  COMMERCE_TEXT_FOOTER_MENU_1: '文章分類', // COMMERCE主题页脚左侧菜单标题1
+  COMMERCE_TEXT_FOOTER_MENU_2: '手冊地圖', // COMMERCE主题页脚左侧菜单标题2
 
   COMMERCE_FOOTER_RIGHT_IMG_URL: 'https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F58a77b77-0672-454d-899a-8f0d3576b8f1%2Fda5aee8a-b0fc-4843-9c0d-adaadddc6c49%2Fpicsee_qrcode_CMUHDNR.png?table=block&id=ad3ecabc-66e2-4fea-9187-039eb3dd0caa&t=ad3ecabc-66e2-4fea-9187-039eb3dd0caa&width=1200&cache=v2', // 显示页脚右侧的图片，通常放二维码
   COMMERCE_FOOTER_RIGHT_TEXT: 'Survival Guide QR code', // 页脚右侧图片下的文字描述
 
-  COMMERCE_HOME_POSTS_COUNT: 9, // 首页展示商品数
+  COMMERCE_HOME_POSTS_COUNT: 12, // 首页展示商品数
   COMMERCE_CONTACT_WHATSAPP_SHOW: false, // 是否展示whatsapp联系按钮 请配置 CONTACT_WHATSAPP
   COMMERCE_CONTACT_TELEGRAM_SHOW: false // 联系栏展示telegram按钮 请配置 CONTACT_TELEGRAM
 }
