bceb55b5ba7628f8be5e93d571b4ae118b5ea98b
Scott Shih <yujen91164@gmail.com>
2024-08-17 16:25:49 +0800

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -36,8 +36,8 @@ function generateLocalesSitemap(link, allPages, locale) {
   }
   const defaultFields = [
     {
-      '<?xml version="1.0" encoding="UTF-8"?>'
-      '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+      <p> <?xml version="1.0" encoding="UTF-8"?>'
+       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> </p>
     }
     { 
       <url>
