732d60058d255f1af8314271ce6bbc33e9347e15
Scott Shih <yujen91164@gmail.com>
2024-08-18 01:39:58 +0800

    Update index.js

--- themes/commerce/index.js
+++ themes/commerce/index.js
@@ -127,11 +127,11 @@ const LayoutIndex = props => {
 
       {/* 首页企业/品牌介绍 这里展示公告 */}
       {notice && (
-        <div id='brand-introduction' className='w-full'>
+         <div id='brand-introduction' className='w-full'>
           <div className='w-full text-center text-4xl font-bold pt-12'>
             {notice.title}
           </div>
-          <NotionPage post={notice} className='text-2xl text-justify' />
+          <NotionPage post={notice} className='dark:text-gray-300text-2xl text-justify' />
         </div>
       )}
       
