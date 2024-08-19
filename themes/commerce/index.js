50b2103bc4d9188e6eb880e3f23df539104e381b
Scott Shih <yujen91164@gmail.com>
2024-08-19 15:31:24 +0800

    Update index.js

--- themes/commerce/index.js
+++ themes/commerce/index.js
@@ -28,6 +28,7 @@ import SlotBar from './components/SlotBar'
 import TagItemMini from './components/TagItemMini'
 import TocDrawer from './components/TocDrawer'
 import { Style } from './style'
+import OneAD from './components/OneAD'
 
 
 /**
@@ -134,22 +135,8 @@ const LayoutIndex = props => {
         </div>
       )}
       
-      {/* OneAds */}
-      <div id="div-onead-draft"> </div>
-      <script type="text/javascript">
-          var custom_call = function (params) {
-                  if (params.hasAd) {console.log('TD has AD') } else {console.log('TD AD Empty')}
-                  }
-            ONEAD_TEXT = {};
-            ONEAD_TEXT.pub = {};
-            ONEAD_TEXT.pub.uid = "2000285";
-            ONEAD_TEXT.pub.slotobj = document.getElementById("div-onead-draft");
-            ONEAD_TEXT.pub.player_mode = "text-drive";
-            ONEAD_TEXT.pub.queryAdCallback = custom_call;
-            window.ONEAD_text_pubs = window.ONEAD_text_pubs || [];
-            ONEAD_text_pubs.push(ONEAD_TEXT);
-          </script>
-          <script src="https://ad-specs.guoshipartners.com/static/js/ad-serv.min.js"></script>
+      {/* OneAD */}
+      <OneAD {...props} />
       
       {/* 产品中心 */}
       <ProductCenter {...props} />
