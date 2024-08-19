4c637fc92b690ba4d48d3f5708ce0938e88b72f9
Scott Shih <yujen91164@gmail.com>
2024-08-19 12:38:58 +0800

    Update index.js

--- themes/commerce/index.js
+++ themes/commerce/index.js
@@ -252,7 +252,7 @@ const LayoutSlug = props => {
             className='overflow-x-auto flex-grow mx-auto md:w-full md:px-5 '>
             {/* 预览区块 */}
 
-/*          {post?.type === 'Post' && (
+/**          {post?.type === 'Post' && (
 *         <div className='flex md:flex-row flex-col w-full justify-between py-4'>
 *                 <div
 *                   id='left-img'
