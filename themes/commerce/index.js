8f127dcd25677ebe46fc60dacc4f033e830a19d1
Scott Shih <yujen91164@gmail.com>
2024-08-20 13:48:33 +0800

    Update index.js

--- themes/commerce/index.js
+++ themes/commerce/index.js
@@ -143,14 +143,6 @@ const LayoutIndex = props => {
         console.log('TD AD Empty')
       }
     }
-  ONEAD_TEXT = {};
-  ONEAD_TEXT.pub = {};
-  ONEAD_TEXT.pub.uid = "2000285";
-  ONEAD_TEXT.pub.slotobj = document.getElementById("div-onead-draft");
-  ONEAD_TEXT.pub.player_mode = "text-drive";
-  ONEAD_TEXT.pub.queryAdCallback = custom_call;
-  window.ONEAD_text_pubs = window.ONEAD_text_pubs || [];
-  ONEAD_text_pubs.push(ONEAD_TEXT);
       </script>
       <script src="https://ad-specs.guoshipartners.com/static/js/ad-serv.min.js"></script>
       
