9e5e30645cf8b86e16e9a60e8724ab0b88452b70
Scott Shih <yujen91164@gmail.com>
2024-08-22 13:16:18 +0800

    Update index.js

--- themes/commerce/index.js
+++ themes/commerce/index.js
@@ -134,18 +134,7 @@ const LayoutIndex = props => {
         </div>
       )}
       
-      <div id="div-onead-draft"></div>
-      <script type="text/javascript">  
-  ONEAD_TEXT = {};
-  ONEAD_TEXT.pub = {};
-  ONEAD_TEXT.pub.uid = "2000285";
-  ONEAD_TEXT.pub.slotobj = document.getElementById("div-onead-draft");
-  ONEAD_TEXT.pub.player_mode = "text-drive";
-  ONEAD_TEXT.pub.queryAdCallback = custom_call;
-  window.ONEAD_text_pubs = window.ONEAD_text_pubs || [];
-  ONEAD_text_pubs.push(ONEAD_TEXT);
-      </script>
-      <script src="https://ad-specs.guoshipartners.com/static/js/ad-serv.min.js"></script>
+
       
       {/* 产品中心 */}
       <ProductCenter {...props} />
