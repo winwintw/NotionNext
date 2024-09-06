3a6a0c5c22f4709f63242185f59f60d4ee3fdd6a
Scott Shih <yujen91164@gmail.com>
2024-09-07 02:27:24 +0800

    Update Footer.js

--- themes/commerce/components/Footer.js
+++ themes/commerce/components/Footer.js
@@ -222,7 +222,7 @@ const Footer = props => {
           {/* 右边公司名字 */}
           <div className='md:text-right'>
             <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>
-              {siteConfig('TITLE')} //{siteConfig('BIO')}//
+              {siteConfig('TITLE')} {/*siteConfig('BIO')*/}
             </h1>
             <h2> {siteConfig('DESCRIPTION')}</h2>
             {/* 可选备案信息 */}
