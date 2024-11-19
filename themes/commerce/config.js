bc930dd1f2acd6af367dac57d0821971971cd075
Scott Shih <yujen91164@gmail.com>
2024-11-19 22:51:47 +0800

    Update config.js
    展示文章 12->18

--- themes/commerce/config.js
+++ themes/commerce/config.js
@@ -12,7 +12,7 @@ const CONFIG = {
   COMMERCE_FOOTER_RIGHT_IMG_URL: 'https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F58a77b77-0672-454d-899a-8f0d3576b8f1%2Fbc0ebe9d-f388-47fc-910c-8c5720e3a84c%2FIMG_0918.png?table=block&id=2f26ce27-8df4-4786-8a38-665268426901&t=2f26ce27-8df4-4786-8a38-665268426901&width=200&cache=v2', // 显示页脚右侧的图片，通常放二维码
   COMMERCE_FOOTER_RIGHT_TEXT: 'Survival Guide QR code', // 页脚右侧图片下的文字描述
 
-  COMMERCE_HOME_POSTS_COUNT: 12, // 首页展示商品数
+  COMMERCE_HOME_POSTS_COUNT: 18, // 首页展示商品数
   COMMERCE_CONTACT_WHATSAPP_SHOW: false, // 是否展示whatsapp联系按钮 请配置 CONTACT_WHATSAPP
   COMMERCE_CONTACT_TELEGRAM_SHOW: false // 联系栏展示telegram按钮 请配置 CONTACT_TELEGRAM
 }
